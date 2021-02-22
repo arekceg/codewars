69d8e6178dc1a29ec5b9f687a11b745f2eaf18f3
aceglowski <arekceg@protonmail.com>
2021-02-22 22:40:04 +0100

    CaesarCipher
    * Start the modified CaesarCipher kata

--- src/main/java/arekceg/CaesarCipher.java
+++ src/main/java/arekceg/CaesarCipher.java
@@ -1,2 +1,61 @@
-package arekceg;public class CaesarCipher {
+package arekceg;
+
+import java.util.ArrayList;
+import java.util.List;
+//https://www.codewars.com/kata/5508249a98b3234f420000fb/train/java
+public class CaesarCipher {
+    public static List<String> movingShift(String s, int shift) {
+        final char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            chars[i] = shiftUnicode(chars[i], shift++);
+        }
+        final String encodedString = String.valueOf(chars).intern();
+        final List<String> split = splitInputStringIntoFiveParts(encodedString);
+        return null;
+    }
+
+    private static List<String> splitInputStringIntoFiveParts(String s) {
+        final int stringLength = s.length();
+        if (stringLength % 5 == 0) return splitEqually(s);
+
+        return null;
+    }
+
+    public static String demovingShift(List<String> s, int shift) {
+        return null;
+    }
+
+    private static char shiftUnicode(char ch, int shift) {
+        if (!Character.isLetter(ch)) return 0;
+        if (Character.isLowerCase(ch)) {
+            return shiftLowerCase(ch, shift);
+        } else {
+            return shiftUpperCaseUnicode(ch, shift);
+        }
+    }
+
+    private static char shiftUpperCaseUnicode(char ch, int shift) {
+        if (ch + shift <= 'Z') {
+            return (char) (ch + shift);
+        } else {
+            return (char) (ch - (26 - shift));
+        }
+    }
+
+    private static char shiftLowerCase(char ch, int shift) {
+        if (ch + shift <= 'z') {
+            return (char) (ch + shift);
+        } else {
+            return (char) (ch - (26 - shift));
+        }
+    }
+
+    private static List<String> splitEqually(String s) {
+        final List<String> split = new ArrayList<>();
+        for (int i = 0; i < s.length(); i += s.length() / 5) {
+            split.add(s.substring(i, i + s.length() / 5));
+        }
+        return split;
+    }
+
 }
--- src/test/java/arekceg/CaesarCipherTest.java
+++ src/test/java/arekceg/CaesarCipherTest.java
@@ -1,6 +1,35 @@
 package arekceg;
 
-import static org.junit.jupiter.api.Assertions.*;
-class CaesarCipherTest {
-  
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class CaesarCipherTest {
+
+    @Test
+    public void test1() {
+        String u = "I should have known that you would have a perfect answer for me!!!";
+        List<String> v = Arrays.asList("J vltasl rlhr ", "zdfog odxr ypw", " atasl rlhr p ", "gwkzzyq zntyhv", " lvz wp!!!");
+        assertEquals(v, CaesarCipher.movingShift(u, 1));
+        assertEquals(u, CaesarCipher.demovingShift(CaesarCipher.movingShift(u, 1), 1));
+    }
+
+//    @Test
+//    public void stringSplitTest() {
+//        final String testString = "123456789123456";
+//        List<String> testList = Arrays.asList("123", "456", "789", "123", "456");
+//        assertTrue(compareListContents(testList, CaesarCipher.splitEqually(testString)));
+//    }
+//
+//    private boolean compareListContents(List<String> list1, List<String> list2) {
+//        if (list1.size() != list2.size()) return false;
+//        for (int i = 0; i < list1.size(); i++) {
+//            if (!list1.get(i).equals(list2.get(i))) return false;
+//        }
+//        return true;
+//    }
 }
