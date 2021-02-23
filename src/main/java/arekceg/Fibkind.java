9da2fb66fdcbcd94f4180cfb835b0b485b7a1074
aceglowski <arekceg@protonmail.com>
2021-02-23 22:18:37 +0100

    Fibkind kata
    * Add Fibkind kata

---
+++ src/main/java/arekceg/Fibkind.java
@@ -0,0 +1,13 @@
+package arekceg;
+
+//https://www.codewars.com/kata/5772382d509c65de7e000982/train/java
+class Fibkind {
+
+    public static long lengthSupUK(int n, int k) {
+        // your code
+    }
+
+    public static long comp(int n) {
+        // your code
+    }
+}
